33d2b776022e83e7f03edf8e29d4108dcf303181
gerasimovicc <gerasimovicc@bk.ru>
2023-08-24 12:14:59 +0300

    Fixed

--- scripts/tabs.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('faq').addEventListener('click', function () {
-	fetch('partial/faq.html')
-		.then(response => response.text())
-		.then(data => {
-			a = document.getElementById('content');
-			a.innerHTML = data;
-		});
-});
